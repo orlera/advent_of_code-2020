d2adbcd8de030da0c79a403d828f2473ca958f05
Andrea Orler <andrea@codegram.com>
2020-12-03 18:13:44 +0100

    day3 - part2

--- lib/day3/day3.ex
+++ lib/day3/day3.ex
@@ -6,18 +6,18 @@ defmodule V2020.Day3 do
   def solution_part1() do
     @input_file_part1
     |> parse_input()
-    |> Enum.reduce({0, 0}, fn element, {_tree_number, x_position} = acc ->
-      element
-      |> String.at(x_position |> rem(@line_length))
-      |> maybe_add_tree(acc)
-    end)
+    |> calculate_trees_in_slope({3, 1})
     |> IO.inspect()
   end
 
   def solution_part2() do
+    slope =
     @input_file_part2
     |> parse_input()
-    |> Enum.count()
+
+    [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+    |> Enum.map(& slope |> calculate_trees_in_slope(&1))
+    |> Enum.reduce(1, fn {trees, _}, acc -> trees * acc end)
     |> IO.puts()
   end
 
@@ -27,8 +27,20 @@ defmodule V2020.Day3 do
     |> String.split("\n")
   end
 
-  defp maybe_add_tree("#", {trees, x_position}) do
-    {trees + 1, x_position + 3}
+  def calculate_trees_in_slope(slope, {right_steps, down_steps}) do
+    slope
+    |> Enum.with_index()
+    |> Enum.reduce({0, 0}, fn {element, y_position}, {_tree_number, x_position} = acc ->
+      if rem(y_position, down_steps) == 0 do
+        element
+        |> String.at(x_position |> rem(@line_length))
+        |> maybe_add_tree(acc, right_steps)
+      else
+        acc
+      end
+    end)
   end
-  defp maybe_add_tree(_, {trees, x_position}), do: {trees, x_position + 3}
+
+  defp maybe_add_tree("#", {trees, x_position}, right_steps), do: {trees + 1, x_position + right_steps}
+  defp maybe_add_tree(_, {trees, x_position}, right_steps), do: {trees, x_position + right_steps}
 end
