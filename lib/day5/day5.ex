f85859ca2fde1f3da2ceaf356843f234b40b1540
Andrea Orler <andrea@codegram.com>
2020-12-05 13:49:36 +0100

    day5 - part1

---
+++ lib/day5/day5.ex
@@ -0,0 +1,50 @@
+defmodule V2020.Day5 do
+  @input_file_part1 "lib/day5/input.txt"
+  @input_file_part2 "lib/day5/input.txt"
+
+  def solution_part1() do
+    @input_file_part1
+    |> parse_input()
+    |> Enum.map(& calculate_seat(&1))
+    |> Enum.max_by(& &1[:id])
+    |> IO.inspect()
+  end
+
+  def solution_part2() do
+    @input_file_part2
+    |> parse_input()
+    # |> IO.inspect()
+  end
+
+  defp parse_input(file_path) do
+    file_path
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(& String.split_at(&1, 7))
+  end
+
+  defp calculate_seat({row_letter, column_letter}) do
+    {row, _} = row_letter |> row_letter_to_number()
+    {column, _} = column_letter |> column_letter_to_number()
+
+    %{
+      row: row,
+      column: column,
+      id: row * 8 + column
+    }
+  end
+
+  defp row_letter_to_number(row_letters) do
+    row_letters
+    |> String.replace("F", "0")
+    |> String.replace("B", "1")
+    |> Integer.parse(2)
+  end
+
+  defp column_letter_to_number(column_letters) do
+    column_letters
+    |> String.replace("L", "0")
+    |> String.replace("R", "1")
+    |> Integer.parse(2)
+  end
+end
