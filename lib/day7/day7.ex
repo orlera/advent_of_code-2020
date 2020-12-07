4fdf3544b511d1908987156cd5b54211d3df9b4c
Andrea Orler <andrea@codegram.com>
2020-12-07 21:13:02 +0100

    day7 - part1

--- lib/day7/day7.ex
+++ lib/day7/day7.ex
@@ -1,5 +1,5 @@
 defmodule V2020.Day7 do
-  @bag_colors_regex ~r/(\w+\s\w+) bags/
+  @bag_colors_regex ~r/(\w+\s\w+) bag/
   @my_bag_color "shiny gold"
   @input_file_part1 "lib/day7/input.txt"
   @input_file_part2 "lib/day7/input.txt"
@@ -7,7 +7,9 @@ defmodule V2020.Day7 do
   def solution_part1() do
     @input_file_part1
     |> parse_input()
-    |> search_possible_containers(@my_bag_color)
+    |> reverse_graph()
+    |> recursive_containers_for(@my_bag_color, [@my_bag_color])
+    |> Enum.uniq()
     |> Enum.count()
     |> IO.puts()
   end
@@ -23,7 +25,7 @@ defmodule V2020.Day7 do
     |> File.read!()
     |> String.split("\n")
     |> Enum.map(fn rule ->
-      Regex.scan(~r/(\w+\s\w+) bag/, rule)
+      Regex.scan(@bag_colors_regex, rule)
       |> Enum.map(& Enum.slice(&1, -1..-1))
       |> List.flatten()
     end)
@@ -32,7 +34,33 @@ defmodule V2020.Day7 do
     end)
   end
 
-  defp search_possible_containers(rules, color) do
-    []
+  defp reverse_graph(rules) do
+    rules
+    |> Map.keys()
+    |> Enum.map(
+      fn color ->
+        {color, containers_for(rules, color)}
+      end
+    )
+    |> Enum.into(%{})
+  end
+
+  defp containers_for(rules, color) do
+    rules
+    |> Enum.flat_map(fn {container, content} ->
+      if Enum.member?(content, color), do: [container], else: []
+    end)
+  end
+
+  defp recursive_containers_for(rules, color, seen) do
+    contained_by =
+      rules
+      |> Map.get(color)
+
+    contained_by
+    |> Enum.reject(& Enum.member?(seen, &1))
+    |> Enum.flat_map(fn color ->
+      [color] ++ recursive_containers_for(rules, color, seen ++ contained_by)
+    end)
   end
 end
