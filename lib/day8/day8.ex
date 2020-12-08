7d05e4a63d98b779493192574bb4c3508222abfe
Andrea Orler <andrea@codegram.com>
2020-12-08 11:28:34 +0100

    day8 - part2

--- lib/day8/day8.ex
+++ lib/day8/day8.ex
@@ -12,6 +12,8 @@ defmodule V2020.Day8 do
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> change_instructions()
+    |> IO.inspect()
   end
 
   defp parse_input(file_path) do
@@ -30,25 +32,50 @@ defmodule V2020.Day8 do
 
   defp boot_console(instruction_set, {accumulator, position}) do
     case Enum.at(instruction_set, position) do
-      %{visited: true} -> accumulator
-      instruction -> boot_console(
-        mark_step_as_visited(instruction_set, position),
-        execute_instruction(instruction, {accumulator, position})
-      )
+      nil ->
+        accumulator
+
+      %{visited: true} ->
+        {:visited, accumulator}
+
+      instruction ->
+        boot_console(
+          mark_step_as_visited(instruction_set, position),
+          execute_instruction(instruction, {accumulator, position})
+        )
     end
   end
 
-  defp execute_instruction(%{operator: "acc", value: value}, {accumulator, position}) do
-    {accumulator + value, position + 1}
+  defp change_instructions(original_instruction_set) do
+    original_instruction_set
+    |> Enum.with_index()
+    |> Enum.reduce_while(0, fn {_instruction, position}, _ ->
+      result_for_step =
+        original_instruction_set
+        |> List.update_at(position, fn instruction_to_update ->
+          instruction_to_update |> change_instruction()
+        end)
+        |> boot_console({0, 0})
+
+      case result_for_step do
+        {:visited, _} -> {:cont, 0}
+        accumulator -> {:halt, accumulator}
+      end
+    end)
   end
 
-  defp execute_instruction(%{operator: "jmp", value: value}, {accumulator, position}) do
-    {accumulator, position + value}
-  end
+  defp execute_instruction(%{operator: "acc", value: value}, {accumulator, position}),
+    do: {accumulator + value, position + 1}
 
-  defp execute_instruction(%{operator: "nop"}, {accumulator, position}) do
-    {accumulator, position + 1}
-  end
+  defp execute_instruction(%{operator: "jmp", value: value}, {accumulator, position}),
+    do: {accumulator, position + value}
+
+  defp execute_instruction(%{operator: "nop"}, {accumulator, position}),
+    do: {accumulator, position + 1}
+
+  defp change_instruction(%{operator: "jmp"} = instruction), do: %{instruction | operator: "nop"}
+  defp change_instruction(%{operator: "nop"} = instruction), do: %{instruction | operator: "jmp"}
+  defp change_instruction(instruction), do: instruction
 
   defp mark_step_as_visited(instruction_set, position) do
     instruction_set
