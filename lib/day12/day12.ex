2e0479c60135a25ba56f70c36a79c06a9db8b267
Andrea Orler <andrea@codegram.com>
2020-12-12 16:14:32 +0100

    day12 - part2

--- lib/day12/day12.ex
+++ lib/day12/day12.ex
@@ -7,11 +7,15 @@ defmodule V2020.Day12 do
     |> parse_input()
     |> follow_route({0, 0, "E"})
     |> manhattan_distance()
+    |> IO.inspect()
   end
 
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> follow_route({0, 0}, {10, 1})
+    |> manhattan_distance()
+    |> IO.inspect()
   end
 
   defp parse_input(file_path) do
@@ -23,21 +27,43 @@ defmodule V2020.Day12 do
     end)
   end
 
+  defp follow_route([], current_ship_position), do: current_ship_position
+
   defp follow_route([step | rest], current_position) do
     next_position = execute_step(step, current_position)
     follow_route(rest, next_position)
   end
 
-  defp follow_route([], current_position) do
-    current_position
+  defp follow_route([], current_ship_position, _), do: current_ship_position
+
+  defp follow_route([step | rest], current_ship_position, current_waypoint_position) do
+    {next_ship_position, next_waypoint_position} =
+      execute_step(step, current_ship_position, current_waypoint_position)
+
+    follow_route(rest, next_ship_position, next_waypoint_position)
   end
 
   defp execute_step({"E", value}, {x, y, direction}), do: {x + value, y, direction}
   defp execute_step({"W", value}, {x, y, direction}), do: {x - value, y, direction}
   defp execute_step({"N", value}, {x, y, direction}), do: {x, y + value, direction}
   defp execute_step({"S", value}, {x, y, direction}), do: {x, y - value, direction}
-  defp execute_step({"F", value}, {_, _, direction} = current_position), do: execute_step({direction, value}, current_position)
-  defp execute_step(instruction, {x, y, current_direction}), do: {x, y, calculate_direction(instruction, current_direction)}
+
+  defp execute_step({"F", value}, {_, _, direction} = current_position),
+    do: execute_step({direction, value}, current_position)
+
+  defp execute_step(instruction, {x, y, current_direction}),
+    do: {x, y, calculate_direction(instruction, current_direction)}
+
+  defp execute_step({"E", value}, ship, {x, y}), do: {ship, {x + value, y}}
+  defp execute_step({"W", value}, ship, {x, y}), do: {ship, {x - value, y}}
+  defp execute_step({"N", value}, ship, {x, y}), do: {ship, {x, y + value}}
+  defp execute_step({"S", value}, ship, {x, y}), do: {ship, {x, y - value}}
+
+  defp execute_step({"F", value}, {x_ship, y_ship}, {x_waypoint, y_waypoint} = waypoint),
+    do: {{x_waypoint * value + x_ship, y_waypoint * value + y_ship}, waypoint}
+
+  defp execute_step(instruction, ship, waypoint),
+    do: {ship, calculate_waypoint(instruction, waypoint)}
 
   defp calculate_direction({"L", 90}, "N"), do: "W"
   defp calculate_direction({"L", 180}, "N"), do: "S"
@@ -67,7 +93,14 @@ defmodule V2020.Day12 do
   defp calculate_direction({"R", 180}, "S"), do: "N"
   defp calculate_direction({"R", 270}, "S"), do: "E"
 
-  defp manhattan_distance({x, y, _}) do
-    abs(x) + abs(y)
-  end
+  defp calculate_waypoint({_, 180}, {x, y}), do: {-x, -y}
+
+  defp calculate_waypoint({"R", 90}, {x, y}), do: {y, -x}
+  defp calculate_waypoint({"L", 270}, {x, y}), do: {y, -x}
+
+  defp calculate_waypoint({"L", 90}, {x, y}), do: {-y, x}
+  defp calculate_waypoint({"R", 270}, {x, y}), do: {-y, x}
+
+  defp manhattan_distance({x, y, _}), do: abs(x) + abs(y)
+  defp manhattan_distance({x, y}), do: abs(x) + abs(y)
 end
