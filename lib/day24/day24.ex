cf49e5ada2d6170e622d79d0d14794a31e580fbb
Andrea Orler <andrea@codegram.com>
2020-12-24 16:14:37 +0100

    day24 - part2

--- lib/day24/day24.ex
+++ lib/day24/day24.ex
@@ -7,8 +7,7 @@ defmodule V2020.Day24 do
     @input_file_part1
     |> parse_input()
     |> normalize_tiles()
-    |> Enum.frequencies()
-    |> Enum.filter(&(elem(&1, 1) |> rem(2) != 0))
+    |> black_tiles()
     |> length()
     |> IO.inspect()
   end
@@ -16,6 +15,10 @@ defmodule V2020.Day24 do
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> normalize_tiles()
+    |> black_tiles()
+    |> flip_tiles()
+    |> length()
     |> IO.inspect()
   end
 
@@ -56,4 +59,54 @@ defmodule V2020.Day24 do
        directions["se"] - directions["nw"] + directions["e"] - directions["w"]}
     end)
   end
+
+  defp black_tiles(tiles) do
+    tiles
+    |> Enum.frequencies()
+    |> Enum.filter(&(elem(&1, 1) |> rem(2) != 0))
+    |> Enum.map(& elem(&1, 0))
+  end
+
+  defp flip_tiles(black_tiles, day \\ 0) do
+    case day do
+      100 -> black_tiles
+      _ -> do_flip_tiles(black_tiles, day)
+    end
+  end
+
+  defp do_flip_tiles(black_tiles, day) do
+    boundaries(black_tiles)
+    |> create_floor()
+    |> change_tiles(black_tiles)
+    |> flip_tiles(day + 1)
+  end
+
+  defp boundaries(tiles), do: {Enum.map(tiles, & elem(&1, 0)) |> Enum.min_max(), Enum.map(tiles, & elem(&1, 1)) |> Enum.min_max()}
+
+  defp create_floor({{minx, maxx}, {miny, maxy}}), do: for x <- minx-1..maxx+1, y <- miny-1..maxy+1, do: {x, y}
+
+  defp change_tiles(floor, black_tiles), do: Enum.filter(floor, & black_after_change?(&1, black_tiles))
+
+  defp black_after_change?(tile, black_tiles) do
+    case is_black?(tile, black_tiles) do
+      true -> case black_neighbours(tile, black_tiles) do
+        flip when flip == 0 or flip > 2 -> false
+        _ -> true
+      end
+      false -> case black_neighbours(tile, black_tiles) do
+        2 -> true
+        _ -> false
+      end
+    end
+  end
+
+  defp is_black?(tile, black_tiles), do: Enum.member?(black_tiles, tile)
+
+  defp black_neighbours(tile, black_tiles) do
+    tile
+    |> neighbouring_tiles()
+    |> Enum.count(& Enum.member?(black_tiles, &1))
+  end
+
+  defp neighbouring_tiles({x, y}), do: [{x-1, y-1}, {x-1, y}, {x, y-1}, {x, y+1}, {x+1, y}, {x+1, y+1}]
 end
